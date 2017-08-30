a346713a0add17ba783dbd51ed511539a8c4b259
emhuff <emhuff@gmail.com>
2017-08-30 10:50:18 -0400

    initialized injector

---
+++ injector/injector.py
@@ -0,0 +1,17 @@
+import galsim
+import logging
+
+## Class for injecting simulated galaxies into pre-existing images.
+
+
+class AddOnImageBuilder(galsim.config.image_scattered.ScatteredImageBuilder):
+    def setup(self, config, base, image_num, obj_num, ignore, logger):
+        ignore = ignore + ['initial_image']
+        return super(AddOnImageBuilder, self).setup(config, base, image_num, obj_num, ignore, logger)
+    def buildImage(self, config, base, image_num, obj_num, logger):
+        im, cv = super(AddOnImageBuilder, self).buildImage(config, base, image_num, obj_num, logger)
+        initial_image_name = galsim.config.ParseValue(config,'initial_image', base, str)[0]
+        initial_image = galsim.fits.read(initial_image_name)
+        im += initial_image
+        return im, cv
+galsim.config.RegisterImageType('AddOn', AddOnImageBuilder())
