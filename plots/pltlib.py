67ffa9d77ebaae4116035ec5f7c340d749def239
Spencer <spencerweverett@gmail.com>
2018-03-20 15:17:22 -0500

    Created a new plotting library to house many of the common plotting routines for Balrog outputs. #19.

---
+++ plots/pltlib.py
@@ -0,0 +1,21 @@
+#####################################################################
+# Put any of your useful Balrog plotting codes here!
+#
+# Last updated: 3/20/2018
+#
+#####################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Put plots here!
+
+
+# Once we have some developed plots, we can make a standard set of
+# them here and save/display
+def MakePlots():
+    pass
+
+if __name__ == '__main__':
+    ret = RunPlots()
+    sys.exit(ret)
