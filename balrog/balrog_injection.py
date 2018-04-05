1e46abe2d238646760bbd5756a1cc76f2b47a82a
Spencer Everett <severett@des61.fnal.gov>
2018-04-05 18:56:56 -0500

    Preparing for merge.

--- balrog/balrog_injection.py
+++ balrog/balrog_injection.py
@@ -39,7 +39,7 @@ from astropy.table import Table
 import injector
 
 # Use for debugging
-import pudb
+#import pudb
 
 #-------------------------------------------------------------------------------
 # Urgent todo's:
