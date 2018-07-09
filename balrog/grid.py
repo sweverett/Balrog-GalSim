f70fdf6519b0af35e744618aecbf3f6b9ef136b8
Spencer <spencerweverett@gmail.com>
2018-07-08 17:35:03 -0700

    Restructured the `HexGrid` generation to be more efficient, return the correct structure, and remove points that lie outside of the given boundary.

--- balrog/grid.py
+++ balrog/grid.py
@@ -16,6 +16,7 @@ import random as rand
 import os, sys, errno
 import warnings
 from astropy.wcs import WCS
+# import pudb
 
 class Grid(object):
 
@@ -52,7 +53,6 @@ class RectGrid(Grid):
 
         return
 
-# TODO: Complete HexGrid class
 class HexGrid(Grid):
     def __init__(self, grid_spacing, wcs, Npix_x=10000, Npix_y=10000, pixscale=0.2631):
         super(HexGrid, self).__init__(grid_spacing, wcs, Npix_x=Npix_x, Npix_y=Npix_y,
@@ -65,20 +65,53 @@ class HexGrid(Grid):
         im_gs = self.grid_spacing * (1.0 / self.pixscale) # pixels
         self.im_grid_spacing = im_gs
 
-	print 'im_gs:',im_gs
-	print 'self.Npix_x-im_gs'
+        #p = HexGrid.calc_polygons(im_gs, im_gs, self.Npix_x-im_gs, self.Npix_y-im_gs, im_gs)
+        # p = HexGrid.calc_polygons(0, 0, self.Npix_x, self.Npix_y, im_gs)
+        # self.im_pos = HexGrid.polygons2coords(p)
+        self.im_pos = HexGrid.calc_hex_coords(im_gs, im_gs, self.Npix_x-im_gs, self.Npix_y-im_gs, im_gs)
 
-        p = HexGrid.calc_polygons(im_gs, im_gs, self.Npix_x-im_gs, self.Npix_y-im_gs, im_gs)
-        c = HexGrid.polygons2coords(p)
-
-        self.im_ra  = c[:,0]
-        self.im_dec = c[:,1]
+        self.im_ra  = self.im_pos[:,0]
+        self.im_dec = self.im_pos[:,1]
 
         # Get all image coordinate pairs
-        self.im_pos = np.array(np.meshgrid(self.im_ra, self.im_dec)).T.reshape(-1, 2)
         self.pos = self.wcs.wcs_pix2world(self.im_pos, 1)
 
         return
+
+    @classmethod
+    def calc_hex_coords(HexGrid, startx, starty, endx, endy, radius):
+        sl = (2 * radius) * np.tan(np.pi / 6)
+
+        # Geoemtric factors of given hexagon
+        p = sl * 0.5
+        b = radius
+        h = 2 * sl
+        dx = b * 2
+        dy = 3 * p
+
+        row = 1
+
+        xs = []
+        ys = []
+
+        while startx < endx:
+            x = [startx, startx, startx + b, startx + dx, startx + dx, startx + b, startx]
+            xs.append(x)
+            startx += dx
+
+        while starty < endy:
+            if row % 2:
+                y = [starty + p, starty + dy, starty + h, starty + dy, starty + p, starty, starty + p]
+                ys.append(y)
+            starty += dy
+            row += 1
+
+        polygons = [zip(x, y) for x in xs for y in ys]
+        hexgrid = HexGrid.polygons2coords(polygons)
+
+        # Some hexagonal elements go beyond boundary; cut these out
+        indx = np.where( (hexgrid[:,0]<endx) & (hexgrid[:,1]<endy) )
+        return hexgrid[indx]
 
     @classmethod
     def calc_polygons(HexGrid, startx, starty, endx, endy, radius):
