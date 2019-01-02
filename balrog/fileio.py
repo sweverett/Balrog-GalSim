8d2ea4cae7a91b948bd9758ac92423739e9ef1e4
Spencer <spencerweverett@gmail.com>
2019-01-02 13:54:13 -0800

    Added `combine_fits_extension()` here. #38

--- balrog/fileio.py
+++ balrog/fileio.py
@@ -1,3 +1,5 @@
+import numpy as np
+import fitsio
 import os
 import sys
 import errno
@@ -51,5 +53,52 @@ def setup_output_dir(config, tiles):
     # Setup tiles, reals, and bands
     for tile in tiles:
 	pass
+
+    return
+
+def combine_fits_extensions(combined_file, bal_file, orig_file, config=None):
+    '''
+    Combine other needed image extensions (e.g. weight and mask maps).
+    Modified from Nikolay's original version in BalrogPipeline.py.
+    '''
+
+    # Read in simulated image and pre-injection extensions
+    sciIm = fitsio.read(bal_file, ext=0)
+    sciHdr = fitsio.read_header(orig_file, ext=0)
+    wgtIm, wgtHdr = fitsio.read(orig_file, ext=1, header=True)
+    wgt_me_Im, wgt_me_Hdr = fitsio.read(orig_file, ext=2, header=True)
+    mskIm, mskHdr = fitsio.read(orig_file, ext=3, header=True)
+
+    # If injecting on blank images, then set these to some sensible values
+    # pudb.set_trace()
+    if config:
+        if config.inj_objs_only['value'] is True:
+            # TODO: This needs to be generalized for different noise models!
+	    if config.inj_objs_only['noise'] == 'BKG+SKY':
+		noise = sciHdr['SKYSIGMA']
+	    else:
+		noise = np.mean([sciHdr['RDNOISEA'], sciHdr['RDNOISEB']])
+            inv_sky_var = 1.0 / (noise**2)
+            wgtIm.fill(inv_sky_var)
+            wgt_me_Im.fill(inv_sky_var)
+            mskIm.fill(0)
+
+    # Write all 3 extensions in the output file
+    # NOTE: Usually combined_file will be the same name as bal_file,
+    # so we delete the original injection and replace it with the new extensions
+    if os.path.exists(combined_file):
+        os.remove(combined_file)
+
+    fits = fitsio.FITS(combined_file,'rw')
+    fits.write(sciIm, header=sciHdr)
+    fits.write(wgtIm, header=wgtHdr)
+    fits.write(wgt_me_Im, header=wgtHdr)
+    fits.write(mskIm, header=mskHdr)
+
+    # Put back EXTNAME into headers
+    fits[0].write_key('EXTNAME', 'SCI', comment="Extension name")
+    fits[1].write_key('EXTNAME', 'WGT', comment="Extension name")
+    fits[2].write_key('EXTNAME', 'WGT_ME', comment="Extension name")
+    fits[3].write_key('EXTNAME', 'MSK', comment="Extension name")
 
     return
